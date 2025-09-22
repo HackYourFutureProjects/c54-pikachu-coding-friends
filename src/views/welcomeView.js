5db2d912ab8c5fcd93888e201e4e18714080354c
Dmytro-Doronin <doronindmytro623@gmail.com>
2025-09-22 14:28:49 +0200

    chore(BR-3): add scss cleanup

--- src/views/welcomeView.js
+++ src/views/welcomeView.js
@@ -5,10 +5,5 @@ import { START_QUIZ_BUTTON_ID } from '../constants.js';
  * @returns {Element}
  */
 export const createWelcomeElement = () => {
-  const element = document.createElement('div');
-  element.innerHTML = String.raw`
-    <h1>Welcome</h1>
-    <button id="${START_QUIZ_BUTTON_ID}">start quiz</button>
-  `;
-  return element;
+
 };
